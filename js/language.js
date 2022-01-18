e8d5708a4e785e9e9dbfc2c4df13aeb7f38f8695
Metehan-Altuntekin <metehanaltuntekin@hotmail.com>
2022-01-18 21:41:11 +0300

    a little improvement in bg-particle sizes and starting language.js

---
+++ js/language.js
@@ -0,0 +1,10 @@
+/* This is a simple static website language adaption system I created myself.
+    It detects navigator's language and if it is "tr-TR" it converts to page to Turkish.
+*/
+if(navigator.language == "tr-TR"){
+  ChangeLanguage("tr-TR")
+}
+
+function ChangeLanguage(language){
+  document.documentElement.lang = language
+}
