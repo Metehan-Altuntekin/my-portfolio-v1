934bf160c8c88a827948edfffe38124137b716de
Metehan-Altuntekin <metehanaltuntekin@hotmail.com>
2022-01-21 18:40:19 +0300

    created a basic way to change language of site

--- js/language.js
+++ js/language.js
@@ -6,60 +6,39 @@ if (navigator.language == "tr-TR") {
 }
 
 var langPack;
+var previousPack;
 function ChangeLanguage(language) {
   document.documentElement.lang = language //Change document's lang attribute
   var packAdress
   if (language == "tr-TR") {
-    packAdress = "../language-packs/TR.JSON"
+    packAdress = "../language-packs/TR-index-body.html"
   }
 
-  GetLangPack(packAdress).then(function (pack) { ApplyLangPack(pack) })
+  GetLangPack(packAdress)
+    .then(function (pack) { ApplyLangPack(pack) })
+    .then(function () {
+      //Need to create animations again
+      CreateBg(document.getElementById("hero"))
+      CreateBg(document.getElementById("portfolio"))
+      CreateBg(document.getElementById("services"), "white")
+      CreateBg(document.getElementById("about"))
+      CreateBg(document.getElementById("contact"), "white")
+    })
 
 }
 
 async function GetLangPack(address) {
-  let data = await fetch(address)
-    .then(response => response.json())
-    .then(jsonObject => {
-      return jsonObject;
-    });
+  let data =
+    await fetch(address)
+      .then(response => response.text())
+      .then(text => {
+        return text;
+      });
   return (data)
 }
 
 function ApplyLangPack(pack) {
-  console.log(pack)
-  //console.log(Object.entries(pack)[0])
-  for (let a in pack) {
-    if (typeof pack[a] !== 'object') {
-      window[`${document}.${a}`]
-    } else {
-      for (let b in pack[a]) {
-        if (typeof pack[a][b] !== 'object') {
-        } else {
-          for (let c in pack[a][b]) {
-            if (typeof pack[a][b][c] !== 'object') {
-              console.log(`document.${a}.${b}.${c}.${pack[a][b][c]} big chungus`)
-              eval(`document.${a}.${b}.${c}.${pack[a][b][c]}`)
-            } else {
-              for (let d in pack[a][b][c]) {
-                if (typeof pack[a][b][c][d] !== 'object') {
-                  console.log(`document.${a}.${b}.${c}.${d}.${pack[a][b][c][d]} big chungus`)
-                  eval(`document.${a}.${b}.${c}.${d}.${pack[a][b][c][d]}`)
-                } else {
-                  for (let e in pack[a][b][c][d]) {
-                    if (typeof pack[a][b][c][d][e] !== 'object') {
-                      console.log(`document.${a}.${b}.${c}.${d}.${e}.${pack[a][b][c][d][e]} big chungus`)
-                      eval(`document.${a}.${b}.${c}.${d}.${e}.${pack[a][b][c][d][e]}`)
-                    } else {
-
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+  previousPack = document.body
+  document.body.innerHTML = ""
+  document.body.innerHTML += pack
 }
