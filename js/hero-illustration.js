98e607425d8efcea49a99c6edd19905253b5559e
Metehan-Altuntekin <metehanaltuntekin@hotmail.com>
2022-01-28 02:30:09 +0300

    hero animation stops on click

--- js/hero-illustration.js
+++ js/hero-illustration.js
@@ -5,3 +5,26 @@ const craneCarryPlatform = document.getElementById("Crane-Carry-Platform")
 frameCarried.style.transform = "translateX(10%)"
 craneString.style.transform = "translateX(10%)"
 craneCarryPlatform.style.transform = "translateX(10%)" */
+
+
+function StartHeroAnim(){
+  frameCarried.style.animationPlayState = "running"
+  craneString.style.animationPlayState = "running"
+  craneCarryPlatform.style.animationPlayState = "running"
+}
+function StopHeroAnim(){
+  frameCarried.style.animationPlayState = "paused"
+  craneString.style.animationPlayState = "paused"
+  craneCarryPlatform.style.animationPlayState = "paused"
+}
+function StartStopHeroAnim() {
+  if (frameCarried.style.animationPlayState == "running") {
+    StopHeroAnim()
+  } else {
+    StartHeroAnim()
+  }
+}
+/* 
+frameCarried.addEventListener("mouseover", StopHeroAnim)
+frameCarried.addEventListener("mouseout", StartHeroAnim) */
+frameCarried.addEventListener("click", StartStopHeroAnim)
